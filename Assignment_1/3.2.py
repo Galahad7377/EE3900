e60bd3cb6aa3e1a6f57d458e799cf2fcf29baf9f
Aditya <ep20btech11001@iith.ac.in>
2022-08-10 12:11:53 +0530

    ...

--- Assignment_1/3.2.py
+++ Assignment_1/3.2.py
@@ -1,9 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#If using termux
-import subprocess
-import shlex
-#end if
 
 
 
@@ -24,23 +20,8 @@ for n in range(2,k-1):
 		y[n] = -0.5*y[n-1]
 print(y)
 
-#subplots
-plt.subplot(2, 1, 1)
-plt.stem(range(0,6),x)
-plt.title('Digital Filter Input-Output')
-plt.ylabel('$x(n)$')
-plt.grid()# minor
-
-
-plt.subplot(2, 1, 2)
 plt.stem(range(0,k),y)
 plt.xlabel('$n$')
 plt.ylabel('$y(n)$')
-plt.grid()# minor
-
-#If using termux
-# plt.savefig('../figs/xnyn.pdf')
-# plt.savefig('../figs/xnyn.eps')
-# subprocess.run(shlex.split("termux-open ../figs/xnyn.pdf"))
-# #else
+plt.grid()
 plt.show()
