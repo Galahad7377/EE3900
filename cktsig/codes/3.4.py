f5aeed3c9544337bbb06c126d62e48dc27919195
aditya <ep20btech11001@iith.ac.in>
2022-10-11 22:56:42 +0530

    ...

--- cktsig/codes/3.4.py
+++ cktsig/codes/3.4.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def v(t):
-    return (4/3)*(1-np.exp((-1.5e6)*t))
+    return (2/3)*(1+np.exp((-1.5e6)*t))
 
 t = np.linspace(0,7e-6)
 plt.plot(t,v(t))
 plt.grid()
-plt.xlabel('$t(s)$')
+plt.xlabel('$t$')
 plt.ylabel('$v_c$')
-plt.savefig('cktsig/figs/2.7.png')
+plt.savefig('cktsig/figs/3.4.png')
